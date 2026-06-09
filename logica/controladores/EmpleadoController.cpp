018a41dbd02e8951973cee117ce569e4c35db639
pablo <pablo.maidana@estudiantes.utec.edu.uy>
2026-06-09 19:19:28 -0300

    Agregue el EmpleadoController.h y .cpp

--- logica/controladores/EmpleadoController.cpp
+++ logica/controladores/EmpleadoController.cpp
@@ -1,5 +1,37 @@
-//
-// Created by facun on 2/6/2026.
-//
+#include #include "EmpleadoController.h"
 
-#include "EmpleadoController.h"
+using namespace std;
+void EmpleadoController::agregarCliente(Cliente* cliente){
+    clientes.push_back(cliente);
+}
+void EmpleadoController::eliminarCliente(Cliente* cliente){
+    for (auto it = clientes.begin(); it != clientes.end(); ++it) {
+        if (*it == cliente) {
+            clientes.erase(it);
+            break;
+        }
+    }
+}
+Cliente* EmpleadoController::buscarCliente(string nombre){
+    for (int i = 0; i < clientes.size(); i++) {
+        if (clientes[i]->getId() == id) {
+            return clientes[i];
+        }
+    }
+    return nullptr;
+}
+vector<Cliente*> EmpleadoController::listarClientes(){
+    return this->clientes;
+}
+void EmpleadoController::agregarOrdenDeCompra(OrdenDeCompra* orden){
+    ordenesDeCompra.push_back(orden);
+}
+void EmpleadoController::eliminarOrdenDeCompra(OrdenDeCompra* orden){
+    for (int i = 0; i < ordenesDeCompra.size(); i++) {
+        if (ordenesDeCompra[i] == orden) {
+            ordenesDeCompra.erase(ordenesDeCompra.begin() + i);
+            break;
+        }
+    }
+}
+EmpleadoController::EmpleadoController(AdminController* adminController) : adminCtrl(adminController) {}
--- logica/controladores/EmpleadoController.h
+++ logica/controladores/EmpleadoController.h
@@ -1,16 +1,22 @@
-//
-// Created by facun on 2/6/2026.
-//
+#ifndef EMPLEADOCONTROLLER_H
+#define EMPLEADOCONTROLLER_H
+#include "Cliente.h"
+#include "OrdenDeCompra.h"
+#include "AdminController.h"
 
-#ifndef PALABORATORIO2026_EMPLEADOCONTROLLER_H
-#define PALABORATORIO2026_EMPLEADOCONTROLLER_H
-
-
-
-class EmpleadoController {
-
+using namespace std;
+class EmpleadoController{
+private:
+    vector<Cliente*> clientes;
+    vector<OrdenDeCompra*> ordenesDeCompra;
+    AdminController* adminCtrl;
+public:
+    void agregarCliente(Cliente* cliente);
+    void eliminarCliente(Cliente* cliente);
+    Cliente* buscarCliente(string nombre);
+    vector<Cliente*> listarClientes();
+    void agregarOrdenDeCompra(OrdenDeCompra* orden);
+    void eliminarOrdenDeCompra(OrdenDeCompra* orden);
+    EmpleadoController(AdminController* adminController);
 };
-
-
-
-#endif //PALABORATORIO2026_EMPLEADOCONTROLLER_H
+#endif
